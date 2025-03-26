80eab60b711c5e70be7a319b0ebe6d90287a535f
juancaparra <parraospinajuancamilo@gmail.com>
2025-03-26 08:50:04 -0500

    conexion a base de datos

---
+++ Model/BaseDatos.cs
@@ -0,0 +1,22 @@
+﻿using Modelo.Entities;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class BaseDatos:ConexionMySql
+    {
+        public int GuardarProducto(string nombre, string descripcion, string autor)
+        {
+            MySqlCommand cmd = GetConnection().CreateCommand();
+            cmd.CommandText = "INSERT INTO libros (nombre,descripcion,autor) VALUES ('" + nombre + "','" + descripcion + "','" + autor + "')";
+            int filasAfectadas = cmd.ExecuteNonQuery();
+
+            return filasAfectadas;
+        }
+    }
+}
---
+++ Model/ConexionMySql.cs
@@ -0,0 +1,38 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class ConexionMySql
+    {
+        public MySqlConnection connection;
+        private string cadenaConexion;
+
+        public ConexionMySql()
+        {
+            cadenaConexion = "Database=TiendaVeloz;Datasource=localhost;User Id=root;Password=mzf2casa18";
+            connection = new MySqlConnection(cadenaConexion);
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            try
+            {
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return connection;
+        }
+    }
+}
