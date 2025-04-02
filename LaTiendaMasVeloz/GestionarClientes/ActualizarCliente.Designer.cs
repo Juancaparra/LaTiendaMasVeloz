cf16e2f8f2589c5a39ac5610c5fd3e01e2927e62
Ronald patiño <patinoguevararonald@gmail.com>
2025-04-01 19:25:11 -0500

    cambio actualizar cliente

--- LaTiendaMasVeloz/GestionarClientes/ActualizarCliente.Designer.cs
+++ LaTiendaMasVeloz/GestionarClientes/ActualizarCliente.Designer.cs
@@ -39,12 +39,15 @@
             textBox1 = new TextBox();
             textBox2 = new TextBox();
             lbResultadoActualizar = new Label();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            label6 = new Label();
             SuspendLayout();
             // 
             // lbTelefonoActualizar
             // 
             lbTelefonoActualizar.AutoSize = true;
-            lbTelefonoActualizar.Location = new Point(192, 273);
+            lbTelefonoActualizar.Location = new Point(192, 286);
             lbTelefonoActualizar.Name = "lbTelefonoActualizar";
             lbTelefonoActualizar.Size = new Size(133, 20);
             lbTelefonoActualizar.TabIndex = 44;
@@ -53,7 +56,7 @@
             // lbNombreActualizar
             // 
             lbNombreActualizar.AutoSize = true;
-            lbNombreActualizar.Location = new Point(192, 202);
+            lbNombreActualizar.Location = new Point(192, 233);
             lbNombreActualizar.Name = "lbNombreActualizar";
             lbNombreActualizar.Size = new Size(129, 20);
             lbNombreActualizar.TabIndex = 43;
@@ -62,15 +65,15 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(192, 139);
+            label4.Location = new Point(121, 139);
             label4.Name = "label4";
-            label4.Size = new Size(121, 20);
+            label4.Size = new Size(194, 20);
             label4.TabIndex = 42;
-            label4.Text = "Ingrese la cedula";
+            label4.Text = "Ingrese la cedula del cliente";
             // 
             // btActualizar
             // 
-            btActualizar.Location = new Point(334, 343);
+            btActualizar.Location = new Point(334, 350);
             btActualizar.Name = "btActualizar";
             btActualizar.Size = new Size(94, 29);
             btActualizar.TabIndex = 41;
@@ -115,14 +118,14 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(324, 202);
+            textBox1.Location = new Point(324, 233);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(133, 27);
             textBox1.TabIndex = 45;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(324, 266);
+            textBox2.Location = new Point(324, 279);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(133, 27);
             textBox2.TabIndex = 46;
@@ -130,16 +133,45 @@
             // lbResultadoActualizar
             // 
             lbResultadoActualizar.AutoSize = true;
-            lbResultadoActualizar.Location = new Point(363, 410);
+            lbResultadoActualizar.Location = new Point(363, 417);
             lbResultadoActualizar.Name = "lbResultadoActualizar";
             lbResultadoActualizar.Size = new Size(0, 20);
             lbResultadoActualizar.TabIndex = 47;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(145, 186);
+            label5.Name = "label5";
+            label5.Size = new Size(164, 20);
+            label5.TabIndex = 49;
+            label5.Text = "Ingrese la cedula nueva";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(324, 183);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(133, 27);
+            textBox3.TabIndex = 48;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(463, 183);
+            label6.Name = "label6";
+            label6.Size = new Size(58, 17);
+            label6.TabIndex = 61;
+            label6.Text = "opcional";
             // 
             // ActualizarCliente
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
             Controls.Add(lbResultadoActualizar);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
@@ -170,5 +202,8 @@
         private TextBox textBox1;
         private TextBox textBox2;
         private Label lbResultadoActualizar;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label6;
     }
 }
